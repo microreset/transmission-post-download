48872d219466d5f8963ee4fce8e515990bc5c7bb
Mimi Portable <crosetremi@gmail.com>
2017-04-29 10:29:43 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # transmission-post-download
-Post download script to parse and move transmission torrents
+Post download script to automatically parse and move torrents downloaded with transmission
+
+## Installation
+  - Edit /etc/transmission-daemon/settings.json (or ~/.config/transmission/settings.json) and specify the following parameters according to your configuration:
+> "script-torrent-done-filename": "~/Scripts/post-download.sh",
+> "rpc-enabled": true,
+> "rpc-username": "user",
+> "rpc-password": "password",
+
+  - Edit post-download.conf to match your needs:
+> REMOTEBIN="/usr/bin/transmission-remote"
+> USER="user"
+> PASSWORD="password"
+> BASE="~/Downloads"
+> LOG="~/Downloads/transmission-post-download.log"
+> MOVIEPATH="Films"
+> MUSICPATH="Musique"
+> TVSHOWPATH="Series"                                                                                      
+> EBOOKPATH="eBooks"                                                                                       
+> DEFAULT="torrent"
+
+  - Make the script executable:
+> chmod +x ~/Scripts/post-download.sh
+
+## Usage
+You can run the script interactively to try the regexp by specifying the option -i :
+> ./post-download.sh -i
---
+++ post-download.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+CONFIGFILE="post-download.conf"
+
+if [ -f $CONFIGFILE ]
+then
+  . $CONFIGFILE
+  echo 
+else
+  echo "Config file $CONFIGFILE not found"
+  exit -1
+fi
+
+if [ -f $REMOTEBIN ]
+then
+  REMOTE="$REMOTEBIN -n $LOGIN:$PASSWORD"
+  echo 
+else
+  echo "$REMOTEBIN not found"
+  exit -1
+fi
+
+if [ ! -f $LOG ]
+then
+  touch $LOG
+fi
+
+TORRENT_NAME=$TR_TORRENT_NAME
+echo `date` " Processing: " $TORRENT_NAME >> $LOG;
+ 
+if [ $# -gt 0 ]
+then
+        # Option -i? (Interactif)
+	if [ $1 = "-i" ]
+	then
+		shift;
+		echo "Usage: $0 [id torrent]";
+                # Affichage de la liste des torrents:
+		$REMOTE -l | tail -n100 | egrep -iv '^$|sum';
+		echo -n "Please select torrent ID: "; read TORRENT_ID;
+		TORRENT_NAME=`$REMOTE -t $TORRENT_ID  -i | grep Name: | sed 's/.*Name: //'`;
+	else
+		TORRENT_NAME=$1;
+		TORRENT_ID=`$REMOTE -l | grep $TORRENT_NAME | awk '{print $1 }'`;
+
+	fi
+elif [ $TORRENT_NAME ]
+then
+	TORRENT_ID=`$REMOTE -l | grep $TORRENT_NAME | awk '{print $1 }'`;
+	echo "Torrent id:"$TORRENT_ID >>$LOG;
+else
+	TORRENT_NAME=`$REMOTE -l | grep 100% | tail -n1 | awk '{ print $10 }'`;
+	TORRENT_ID=`$REMOTE -l | grep $TORRENT_NAME | awk '{print $1 }'`;
+fi
+
+echo "Torrent name:"$TORRENT_NAME;
+echo "Torrent id:"$TORRENT_ID;
+
+TORRENT_NAME=`$REMOTE -t $TORRENT_ID -i |grep Name |cut -d ":" -f2`
+
+echo Torrent: $TORRENT_NAME >> $LOG;
+
+case $TORRENT_NAME in
+        # TV Shows
+	$(echo "${TORRENT_NAME}" | egrep -i 'S[0-9][0-9]|saison *[0-9]+|season *[0-9]+'))
+		DEST="$TVSHOWPATH";
+		echo `date` "Directory of $TORRENT_NAME: $DEST" >> $LOG;
+		;;
+        # Movies
+	$(echo "${TORRENT_NAME}" |egrep -i 'DVDR|HDTV|XVID|BDRIP|BRRIP|HDRIP|WEBRIP|BRRIP|Blueray|bluray|720p|1080p|NTSC|*.avi$|*.mkv$'))
+		DEST="$MOVIEPATH";
+		echo `date` "Directory of $TORRENT_NAME: $DEST" >> $LOG;
+		;;
+        # Music
+	$(echo "${TORRENT_NAME}" | egrep -i 'flac|mp3'))
+		DEST="$MUSICPATH";
+		echo `date` "Directory of $TORRENT_NAME: $DEST" >> $LOG;
+		;;
+        # eBooks
+        $(echo "${TORRENT_NAME}" | egrep -i 'epub'))
+		DEST="$EBOOKPATH";
+		echo `date` "Directory of $TORRENT_NAME: $DEST" >> $LOG;
+		;;
+
+        # Default
+	*) DEST="$DEFAULT";
+		echo `date` "$TORRENT_NAME does not match! Sent in $DEST" >> $LOG
+		;;
+esac
+
+$REMOTE -t $TORRENT_ID --move "$DEST"
